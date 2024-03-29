6614a2e3990dc2205b114afcd5de01658d2167ff
Kabimbi Ngoy Arnaud <a.kabimbingoy@stag.technofuturtic.education>
2024-03-29 14:57:10 +0100

    game_loop methode 0.2

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ def game_loop():
     move_left = 10
     color_picked = 0
     master_mind_color = list()
+    running = True
 
     print("[M] Choix du code")
     while color_picked < 4:
         master_mind_color.append(colorPick())
         color_picked += 1
     
-    while move_left > 0:
+    while move_left > 0 and running:
         print("[J] Deviner le code")
-        evaluer(master_mind_color)
+        running = not evaluer(master_mind_color)
         move_left -= 1
 
 if __name__ == "__main__":
