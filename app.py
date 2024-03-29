08be66448ff1a85b5551be404f59b45e36e90be9
Kabimbi Ngoy Arnaud <a.kabimbingoy@stag.technofuturtic.education>
2024-03-29 16:17:42 +0100

    game_loop final

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def game_loop():
 
     print("[M] Choix du code")
     while color_picked < 4:
-        master_mind_color.append(colorPick())
+        master_mind_color.append(colorPick())  
         color_picked += 1
     
     while move_left > 0 and running:
