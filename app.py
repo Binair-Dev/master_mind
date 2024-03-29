89c4790595a985de63bbcc9ff8ff2e328918080d
Brian Van Bellinghen <b_nair.corp@yahoo.com>
2024-03-29 16:42:53 +0100

    conflict fix

--- app.py
+++ app.py
@@ -1,3 +1,43 @@
+def evaluer(couleur):
+    check = []
+    for _ in range(5):
+        check.append(colorPick())
+
+    check1 = set()
+    check2 = []
+    for i, res in enumerate(check):
+        for j,resp in enumerate(couleur):
+            if res[i] == resp[j]:
+                check1.add(res[i])
+            elif res in check:
+                check2.append(res)
+
+    if len(check1) == 3:
+        return True
+    elif check2:
+        return check2
+    else:
+        return False
+
+def game_loop():
+    move_left = 10
+    color_picked = 0
+    master_mind_color = list()
+    running = True
+
+    print("[M] Choix du code")
+    while color_picked < 4:
+        master_mind_color.append(colorPick())  
+        color_picked += 1
+    
+    while move_left > 0 and running:
+        print("[J] Deviner le code")
+        running = not evaluer(master_mind_color)
+        move_left -= 1
+
+if __name__ == "__main__":
+    game_loop()
+
 def colorPick():
     userInput = input("Veuillez sélectionner une couleur:")
     userInput.strip().lower()
